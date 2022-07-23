9946ae2a887353ad0ae86af7bf913582975d22c2
David Wolf <68775926+devidw@users.noreply.github.com>
2022-07-23 09:30:56 +0000

    closes #2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import './index.styl'
+import "./index.styl"
 
 class BrktFldg {
-
     options = {
-        openChar: '(', // Replaces the original opening bracket
-        closeChar: ')', // Replaces the original closing bracket
-        placeholder: '…', // Displayed as text for the toggle to expand and collapse the content
+        needles: ["(", ")"], // The characters to search for
+        needleRegex: /\((?!\))|(?<!\()\)/g, // The regex to test text nodes for
+        replacements: ["(", ")"], // Replaces the original opening and closing bracket characters with the given ones
+        placeholder: "…", // Displayed as text for the toggle to expand and collapse the content
         shortMaxWords: 1, // Number of words to determine if the content is short, quick expanding is possible on short conent when enabled and the word count is not displayed on short content
         wordCount: true, // Show word count
         expandShortOnHover: true, // Expand brackets with short content on hover
-        ignoreTags: ['script', 'style', 'pre', 'code', 'brktfldg-container',] // Ignore these tags
+        ignoreTags: ["script", "style", "pre", "code", "brktfldg-container"], // Ignore these tags
     }
 
     constructor(el, options = {}) {
@@ -21,9 +21,9 @@ class BrktFldg {
 
     /**
      * Find all brackets in the given element
-     * 
+     *
      * Loop through the child nodes and build an array of the following structure:
-     * [ { "char": "(|)", "node": <node>, "offset": <offset> }, ... ] 
+     * [ { "char": "(|)", "node": <node>, "offset": <offset> }, ... ]
      */
     getBrackets(el) {
         const brackets = []
@@ -35,7 +35,7 @@ class BrktFldg {
         for (const child of el.childNodes) {
             if (child.nodeType === Node.TEXT_NODE) {
                 let txt = child.nodeValue
-                const matches = txt.match(/\(|\)/g)
+                const matches = txt.match(this.options.needleRegex)
 
                 if (!matches) {
                     continue
@@ -53,15 +53,15 @@ class BrktFldg {
                     brackets.push({
                         char: match,
                         node: child,
-                        offset: originalIndex
+                        offset: originalIndex,
                     })
                 }
             }
             // In case of an element, recurse
             else if (
-                child.nodeType === Node.ELEMENT_NODE
-                && child.hasChildNodes()
-                && !this.options.ignoreTags.includes(child.tagName.toLowerCase())
+                child.nodeType === Node.ELEMENT_NODE &&
+                child.hasChildNodes() &&
+                !this.options.ignoreTags.includes(child.tagName.toLowerCase())
             ) {
                 brackets.push(...this.getBrackets(child))
             }
@@ -72,27 +72,10 @@ class BrktFldg {
 
     /**
      * Get pairs of brackets from the given found brackets
-     * 
+     *
      * [ [ {}, {}, ], [ {}, {}, ], ... ]
      */
     getPairs(brackets) {
-
-        /**
-         * Validate that matches are "(" and ")" after each other
-         */
-        function hasOnlyPairs(arr) {
-            return arr.reduce((acc, curr, i) => {
-                if (i % 2 === 0) {
-                    return acc && curr.char === '('
-                }
-                return acc && curr.char === ')'
-            }, true)
-        }
-
-        // if (!hasOnlyPairs(brackets)) {
-        //     return []
-        // }
-
         /**
          * Transform: [0,1,2,3] => [[0,1],[2,3]]
          */
@@ -104,16 +87,25 @@ class BrktFldg {
             return pairs
         }
 
-        return brackets2pairs(brackets)
+        const paris = brackets2pairs(brackets)
+
+        // Remove if the pair is not valid, check on length and if the characters match
+        const filtered = paris.filter((pair) => {
+            return typeof pair[0] === "object"
+                && typeof pair[1] === "object"
+                && pair[0].char === this.options.needles[0]
+                && pair[1].char === this.options.needles[1]
+        })
+
+        return filtered
     }
 
     /**
      * Apply needed changes to the DOM
-     * 
+     *
      * We add additional markup around the brackets and their contents to achieve the desired functionality for callapsing and expanding them
      */
     wrapBrackets(pairs) {
-
         if (!pairs.length) {
             return
         }
@@ -130,28 +122,69 @@ class BrktFldg {
             range.setStart(open.node, open.offset)
             range.setEnd(close.node, close.offset + 1)
 
-            const container = document.createElement('brktfldg-container')
-            container.setAttribute('data-brktfldg-open-char', this.options.openChar)
-            container.setAttribute('data-brktfldg-close-char', this.options.closeChar)
-            const toggle = document.createElement('brktfldg-toggle')
-            toggle.setAttribute('data-brktfldg-placeholder', this.options.placeholder)
-            const content = document.createElement('brktfldg-content')
+            const container = document.createElement("brktfldg-container")
+            container.setAttribute(
+                "data-brktfldg-open-char",
+                this.options.replacements[0]
+            )
+            container.setAttribute(
+                "data-brktfldg-close-char",
+                this.options.replacements[1]
+            )
+            const toggle = document.createElement("brktfldg-toggle")
+            toggle.setAttribute(
+                "data-brktfldg-placeholder",
+                this.options.placeholder
+            )
+            const content = document.createElement("brktfldg-content")
 
-            range.surroundContents(content)
+            try {
+                // range.surroundContents(content)
+                content.appendChild(range.extractContents())
+                range.insertNode(content)
+            } catch (e) {
+                console.error(e)
+                continue
+            }
 
             content.parentElement.insertBefore(container, content)
             container.appendChild(toggle)
             container.appendChild(content)
 
-            // Remove "(" and ")" from the content, first and last characters
-            content.firstChild.nodeValue = content.firstChild.nodeValue.slice(1)
-            content.lastChild.nodeValue = content.lastChild.nodeValue.slice(0, -1)
+            // Remove "(" from the content, first character
+            function rmOpenCharHelper(textNode, char) {
+                if (textNode.nodeValue.charAt(0) === char) {
+                    textNode.nodeValue = textNode.nodeValue.slice(1)
+                }
+            }
+
+            // Remove ")" from the content, last character
+            function rmCloseCharHelper(textNode, char) {
+                if (textNode.nodeValue.charAt(textNode.nodeValue.length - 1) === char) {
+                    textNode.nodeValue = textNode.nodeValue.slice(0, -1)
+                }
+            }
+
+            function applyRmToEl(el, callback, char) {
+                if (el.nodeType === Node.TEXT_NODE) {
+                    callback(el, char)
+                }
+                // If the content does not start with a text node we have to find the bracket in the first child node, go deeper if necessary
+                else if (el.nodeType === Node.ELEMENT_NODE) {
+                    for (const child of el.childNodes) {
+                        applyRmToEl(child, callback, char)
+                    }
+                }
+            }
+
+            applyRmToEl(content.firstChild, rmOpenCharHelper, this.options.replacements[0])
+            applyRmToEl(content.lastChild, rmCloseCharHelper, this.options.replacements[1])
         }
     }
 
     /**
      * Process each bracket's content and calculate its word count
-     * 
+     *
      * The word count can be configured to be displayed inside the toggle element instead of the placeholder, when the brackets are closed
      * The short count is also used to determine if the bracket's content is short and therefore the word count indicator should be displayed and quick expanded on hover is possible if enabled
      */
@@ -182,17 +215,17 @@ class BrktFldg {
             const wordCount = getWordCount(content.innerHTML)
 
             if (this.options.wordCount) {
-                toggle.setAttribute('data-brktfldg-word-count', wordCount)
+                toggle.setAttribute("data-brktfldg-word-count", wordCount)
             }
 
             const isShort = wordCount <= this.options.shortMaxWords
             if (isShort) {
-                container.classList.add('brktfldg-container--short')
+                container.classList.add("brktfldg-container--short")
             }
         })
     }
 
-    /** 
+    /**
      * Add toggle functionality to the toggle elements
      */
     addEventListeners() {
@@ -200,17 +233,25 @@ class BrktFldg {
         toggles.forEach((toggle) => {
             const container = toggle.parentElement
 
-            toggle.addEventListener('click', () => {
-                container.classList.toggle('brktfldg-container--open')
+            toggle.addEventListener("click", (e) => {
+                e.stopPropagation()
+                e.preventDefault()
+                container.classList.toggle("brktfldg-container--open")
             })
 
             if (this.options.expandShortOnHover) {
-                const isShort = container.classList.contains('brktfldg-container--short')
+                const isShort = container.classList.contains(
+                    "brktfldg-container--short"
+                )
 
                 if (isShort) {
-                    toggle.addEventListener('mouseover', () => {
-                        if (!container.classList.contains('brktfldg-container--open')) {
-                            container.classList.add('brktfldg-container--open')
+                    toggle.addEventListener("mouseover", () => {
+                        if (
+                            !container.classList.contains(
+                                "brktfldg-container--open"
+                            )
+                        ) {
+                            container.classList.add("brktfldg-container--open")
                         }
                     })
                 }
@@ -226,7 +267,7 @@ class BrktFldg {
             return
         }
 
-        const isCollapsed = this.el.querySelector('brktfldg-container')
+        const isCollapsed = this.el.querySelector("brktfldg-container")
 
         if (isCollapsed) {
             return
